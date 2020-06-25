a985a4687988e2f3da3a96cc1deaa9d6b9ba47ff
Jared Gurr <jaredgurr@pop-os.localdomain>
2020-06-25 06:09:48 -0700

    onpageload added.

---
+++ 03_java_backend/src/main/java/com/epicGamecraft/dungeonCrawlDepths/OnPageLoadVerticle.java
@@ -0,0 +1,22 @@
+package com.epicGamecraft.dungeonCrawlDepths;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class OnPageLoadVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+	  checkSessionId();
+  }
+  
+  public int checkSessionId() {
+	  
+	  id == null ? createSessionId() : return id; 
+  }
+  
+  public static int createSessionId() {
+	  int id = n + 1;
+	  return id;
+  }
+}

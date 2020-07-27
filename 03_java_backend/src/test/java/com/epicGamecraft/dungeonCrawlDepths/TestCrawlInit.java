ebba4706bc0d47ebb0aa930988d124938144b04b
Jared Gurr <jared@gurrfamily.com>
2020-07-27 08:28:49 -0700

    minor changes

--- 03_java_backend/src/test/java/com/epicGamecraft/dungeonCrawlDepths/TestCrawlInit.java
+++ 03_java_backend/src/test/java/com/epicGamecraft/dungeonCrawlDepths/TestCrawlInit.java
@@ -6,6 +6,8 @@ import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,27 +18,36 @@ import static com.epicGamecraft.dungeonCrawlDepths.BusEvent.*;
 @ExtendWith(VertxExtension.class)
 public class TestCrawlInit {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new HttpServerVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestCrawlInit.class);
+	
 
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    vertx.deployVerticle(new HttpServerVerticle(), testContext.succeeding(id -> testContext.completeNow()));
     testContext.completeNow();
   }
   
   
   @Test
   void login(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    vertx.deployVerticle(new UserVerticle(), Handler<AsyncResult<String>>() {
+    	
+    });
+    vertx.deployVerticle(new CouchbaseVerticle(), testContext.succeeding(id -> testContext.completeNow()));
 	//add code here to create a document with usernameOrEmail and assign it Jared Gurr, and also do password.
     vertx.eventBus().request(userLogin.name(), "{ 'usernameOrEmail' : 'Jared Gurr', 'password' "
-    		+ ": 'hashpassword' }");
+    		+ ": 'hashpassword' }", ar -> {
+    			if (ar.succeeded()) {
+    				LOGGER.debug("Test Verticle received reply: " + ar.result().body());
+    			}
+    		});
+    testContext.completeNow();
   }
 
-  @Test
-  void failedLoginAttempt(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    vertx.eventBus().request(couchbaseQuery.name(), "select name from registration where name='jared' "
-    		+ "and ");
-  }
+//  @Test
+//  void failedLoginAttempt(Vertx vertx, VertxTestContext testContext) throws Throwable {
+//    vertx.eventBus().request(couchbaseQuery.name(), "select name from registration where name='jared' "
+//    		+ "and ");
+//    testContext.completeNow();
+//  }
 }

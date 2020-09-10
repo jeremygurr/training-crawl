8a7de57bfe1c7e9a6f4bc82a457c1fd61eb144ce
Jared Gurr <jared@gurrfamily.com>
2020-09-10 08:23:07 -0700

    Started staticHandler functionality.

--- 03_java_backend/src/main/java/com/epicGamecraft/dungeonCrawlDepths/HttpServerVerticle.java
+++ 03_java_backend/src/main/java/com/epicGamecraft/dungeonCrawlDepths/HttpServerVerticle.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import io.vertx.reactivex.core.Vertx.*;
 import io.reactivex.*;
+import io.reactivex.annotations.Nullable;
 import io.reactivex.disposables.*;
 import io.reactivex.observers.*;
 import io.reactivex.schedulers.*;
@@ -21,6 +22,9 @@ import io.vertx.reactivex.*;
 import io.vertx.reactivex.core.*;
 import io.vertx.reactivex.core.http.*;
 import io.vertx.reactivex.ext.web.*;
+import io.vertx.reactivex.ext.web.handler.SessionHandler;
+import io.vertx.reactivex.ext.web.sstore.LocalSessionStore;
+import io.vertx.reactivex.ext.web.sstore.SessionStore;
 import io.vertx.reactivex.core.buffer.*;
 
 public class HttpServerVerticle extends AbstractVerticle {
@@ -34,9 +38,9 @@ public class HttpServerVerticle extends AbstractVerticle {
     Router router = Router.router(vertx);
 
     router.get("/status").handler(this::statusHandler);
-//		router.get("/static/*").handler(this::staticHandler);
-//		router.route().handler(BodyHandler.create());
-//		router.post("/bus/*").handler(this::busHandler);
+    router.get("/static/*").handler(this::staticHandler);
+    //		router.route().handler(BodyHandler.create());
+    //		router.post("/bus/*").handler(this::busHandler);
     final int port = 8080;
     final Single<HttpServer> rxListen = server
         .requestHandler(router)
@@ -59,4 +63,63 @@ public class HttpServerVerticle extends AbstractVerticle {
 
   }
 
+  private void staticHandler(RoutingContext context) {
+
+    final HttpServerResponse response = context.response();
+    final HttpServerRequest request = context.request();
+    @Nullable
+    String path = request.path();
+    LOGGER.debug("Get " + path);
+    response.end();
+  }
 }
+
+/*
+    try {
+      LOGGER.debug("GET " + path);
+      path = path.substring(1);
+      final InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+      if (stream != null) {
+        final String text = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines()
+            .collect(Collectors.joining("\n"));
+        if (path.endsWith(".html")) {
+          response.putHeader("Content-Type", "text/html");
+        } else if (path.endsWith(".css")) {
+          response.putHeader("Content-Type", "text/css");
+        } else {
+          response.end("<html><body>Error filetype unknown: " + path + "</body></html>");
+        }
+        response.setStatusCode(200);
+        response.end(text);
+      } else {
+        LOGGER.warn("Resource not found: " + path);
+        response.setStatusCode(404);
+        response.end();
+      }
+    } catch (Exception e) {
+      LOGGER.error("Problem fetching static file: " + path, e);
+      response.setStatusCode(502);
+      response.end();
+    }
+  }
+
+@Nullable
+String path = request.path();
+
+Cookie userCookie = context.getCookie("myCookie");
+String cookieValue = userCookie.getValue();
+context.addCookie(Cookie.cookie("othercookie", "somevalue"));
+Session session = context.session();
+SessionStore store = LocalSessionStore.create(vertx);  //Creates session store. Required to create sessionHandler.
+SessionHandler sessionHandler = SessionHandler.create(store);   //Used to configure and set cookies for the session.
+sessionHandler.setSessionCookieName("User123");
+sessionHandler.setSessionCookiePath("/something");
+sessionHandler.setSessionTimeout(5000000); //5 minutes. Not needed since default is 30 min.
+
+String username = session.get("name");
+String webpage = session.get("path");
+
+
+
+
+*/

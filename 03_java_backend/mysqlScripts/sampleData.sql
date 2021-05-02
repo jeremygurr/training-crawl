16fc5bf05b62c4f9e48bc1ac85b80c0e7730ef48
jgurr4 <jared@gurrfamily.com>
2021-05-01 21:58:33 -0700

    updated readme.adoc

--- 03_java_backend/mysqlScripts/sampleData.sql
+++ 03_java_backend/mysqlScripts/sampleData.sql
@@ -1,3 +1,3 @@
-INSERT INTO `player` VALUES (0, 'billybob',174612873,'bob@gmail.com');
+INSERT INTO `player` VALUES (0, 'TheGuy',174612873,'something@gmail.com');
 INSERT INTO `character` VALUES (0,1,'Ross the cold Enchanter',default,default,default);
-INSERT INTO `game` VALUES (0,1,'paused',default,default,null,default,1,default);
+INSERT INTO `game` VALUES (0,1,'paused',default,default,default,default,1,default);
